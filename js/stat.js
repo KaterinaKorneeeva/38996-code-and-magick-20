02229401ee31eacd7a44f933df1602e17e15da2f
Корнеева Екатерина <kokatrn@gmail.com>
2020-06-01 17:41:42 +0300

    fix module2-task1

--- js/stat.js
+++ js/stat.js
@@ -43,8 +43,6 @@ var getColorBar = function (player, i) {
     : getRandomBlueColor() ;
 };
 
-
-
 window.renderStatistics = function(ctx, players, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
