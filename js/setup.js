f8aef4b55e95bd2b60e2eacc3029405bdc42ca24
Корнеева Екатерина <kokatrn@gmail.com>
2020-06-11 23:14:59 +0300

    одеть Надежду

---
+++ js/setup.js
@@ -0,0 +1,76 @@
+'use strict';
+
+var MIN_NAME_LENGTH = 2;
+var MAX_NAME_LENGTH = 25;
+
+var setupOpen = document.querySelector('.setup-open');
+var setup = document.querySelector('.setup');
+var setupClose = setup.querySelector('.setup-close');
+var setupUserName = setup.querySelector('.setup-user-name');
+var userNameInput = document.querySelector('.setup-user-name');
+var setupSubmit = document.querySelector('.setup-submit');
+
+
+var onPopupEscPress = function (evt) {
+  if (evt.key === 'Escape' && event.target !== setupUserName) {
+    evt.preventDefault();
+    closePopup();
+  }
+};
+
+var openPopup = function () {
+  setup.classList.remove('hidden');
+
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function () {
+  setup.classList.add('hidden');
+
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+setupOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.key === 'Enter') {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener('click', function () {
+  closePopup();
+});
+
+setupClose.addEventListener('keydown', function (evt) {
+
+  if (evt.key === 'Enter') {
+    evt.preventDefault();
+    closePopup();
+  }
+});
+
+// Отправка форма по нажатию на ENTER
+setupSubmit.addEventListener('keydown', function (evt) {
+  evt.preventDefault();
+
+// console.log('test', evt.key === 'Enter' && event.target !== setupSubmit);
+//   if (evt.key === 'Enter' && event.target !== setupSubmit) {
+//     setupSubmit.submit();
+//   }
+});
+
+
+userNameInput.addEventListener('input', function () {
+  var valueLength = userNameInput.value.length;
+
+  if (valueLength < MIN_NAME_LENGTH) {
+    userNameInput.setCustomValidity('Ещё ' + (MIN_NAME_LENGTH - valueLength) + ' симв.');
+  } else if (valueLength > MAX_NAME_LENGTH) {
+    userNameInput.setCustomValidity('Удалите лишние ' + (valueLength - MIN_NAME_LENGTH) + ' симв.');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
